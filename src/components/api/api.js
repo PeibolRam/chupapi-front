5b11de9b2e38f771ae023b444fd18c78febb61ec
Agustín Herrera <agustin@dreamsengine.io>
2020-02-14 17:47:08 -0600

    api

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,11 +1,127 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "../../styles/api.css";
 
 export default class Api extends Component {
     render() {
         return (
             <div>
-                Poner la documentación de la API aqui
+                
+    <h1>Documentación APIstear</h1>
+    <div className="wrapper">
+      <h2>Acerca de nosotros</h2>
+      <p>
+        Somos un grupo de jóvenes inovadores que tratan de llevar la tecnología
+        al máximo para que puedas tener a su alcance los mejores tips, recetas y
+        modo de preparación tus cocteles. Esto la finalidad de que no pagues mas y no
+        tengas que salir de casa sólo por querer experimentar la sensación de
+        bebidas exquisitas en tu paladar. Nuestro sentido de creatividad junto
+        con la tecnología nos impulsaron a crear APIstear, una API que te
+        ayuda a crear cocteles que jamás creiste poder preparar tu mismo para
+        tus reuniones, cenas,comidas o fiestas que te hagan ver como un gran
+        anfitrión.
+      </p>
+    </div>
+    <div className="wrapper">
+        <h2>Peticiones disponibles</h2>
+        <h3>GET /drinks/</h3>
+    <p>Con esta petición puedes sacar toda la información de la base</p>
+    <figure className="img_call"><img src="http://165.227.1.54:5000/public/get_drinks.png" /></figure>
+
+    <h3>GET /drinks/nombre/nombre de tu drink</h3>
+    <p>
+      Con esta petición puedes sacar toda la información del drink que
+      buscaste por nombre
+    </p>
+    <figure className="img_call"><img src="http://165.227.1.54:5000/public/get_nombre.jpg" /></figure>
+
+    <h3>GET /drinks/id/id del drink</h3>
+    <p>
+      Con esta petición puedes sacar toda la información del drink que
+      buscaste por ID
+    </p>
+    <figure className="img_call"><img src="http://165.227.1.54:5000/public/get_id.png" /></figure>
+
+    <h3>GET /drinks/base/bebida base</h3>
+    <p>
+      Con esta petición puedes sacar todos los cocteles que usan la base
+      especificada
+    </p>
+    <figure className="img_call"><img src="http://165.227.1.54:5000/public/get_base.png" /></figure>
+
+    <h3>POST /drinks/register/</h3>
+    <p>Con esta petición puedes dar de alta nuevos drinks</p>
+    <figure className="img_call"><img src="http://165.227.1.54:5000/public/post_register.png" /></figure>
+    </div>
+    <div className="wrapper">
+      <h2>Descripción de los elementos</h2>
+      <table className="Bebidas" cellpadding="0" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Elemento</th>
+            <th>Descripción</th>
+            <th>Tipo de dato</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>id</td>
+            <td>Identificador único de la bebida</td>
+            <td>Number</td>
+          </tr>
+          <tr>
+            <td>Nombre</td>
+            <td>Nombre de la bebida</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>ingredientes</td>
+            <td>
+              Regresa un array con la lista de ingredientes. Cada ingrediente es
+              llamado como un objecto
+            </td>
+            <td>array</td>
+          </tr>
+          <tr>
+            <td>Descripción</td>
+            <td>Dato hitórico de la bebida</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Instrucciones</td>
+            <td>Forma magnífica de prepara la bebida</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Situación</td>
+            <td>Recomendación para beberla</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Recomendación</td>
+            <td>Acompañamiento para la degustación</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Origen</td>
+            <td>Lugar de nacimiento</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Imagen</td>
+            <td>Demostración final de tu bebida</td>
+            <td>String</td>
+          </tr>
+          <tr>
+            <td>Base</td>
+            <td>Ingrediente principal de la bebida</td>
+            <td>String</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    
             </div>
         )
     }
 }
+
--- src/components/home/hero/hero.js
+++ src/components/home/hero/hero.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import Best from './best/best'
-import '../../../styles/hero.css'
+import React from "react";
+import Best from "./best/best";
+import "../../../styles/hero.css";
 
 const Hero = () => {
-    return(
-        <div className="hero_chupapi">
-            <div className="desc_chupapi">
-                <h1>APIstear</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil deleniti ratione voluptates repellendus provident delectus optio itaque eos, sed eum, veniam quidem ducimus blanditiis quos commodi repellat voluptatibus, iure sapiente.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint earum adipisci est dolorem, sed sequi a officiis. Ea vitae cum qui dignissimos sed vero, nam totam quasi doloremque dolores dolorem?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur consequatur eaque repellat incidunt maiores veritatis impedit, mollitia obcaecati placeat est totam, optio cum quae, ea veniam fuga itaque rem ut.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente magni magnam blanditiis, commodi eligendi in fugiat similique sequi eius. Libero adipisci error reprehenderit iure possimus magnam aut rem quam consequatur!
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi velit adipisci nesciunt sit non earum, tempora laboriosam possimus incidunt quasi minima reiciendis necessitatibus, deserunt iste distinctio molestiae, ipsam vitae repudiandae.
-                </p>
-            </div>
-            <Best/>
-        </div>
-    )
-}
+  return (
+    <div className="hero_chupapi">
+      <div className="desc_chupapi">
+        <h1>APIstear</h1>
+        <p>
+          Somos un grupo de jóvenes inovadores que tratan de llevar la
+          tecnología al máximo para que puedas tener a su alcance los mejores
+          tips, recetas y modo de preparación tus cocteles. Esto la finalidad de
+          que no pagues mas y no tengas que salir de casa sólo por querer
+          experimentar la sensación de bebidas exquisitas en tu paladar. Nuestro
+          sentido de creatividad junto con la tecnología nos impulsaron a crear
+          APIstear, una API que te ayuda a crear cocteles que jamás creiste
+          poder preparar tu mismo para tus reuniones, cenas,comidas o fiestas
+          que te hagan ver como un gran anfitrión.
+        </p>
+      </div>
+      <Best />
+    </div>
+  );
+};
 
 export default Hero;
